ff4a06dea12550dc646c4ddbd009e6dd2c82af75
James Thibodeau <jthibaudeau@accedian.com>
2019-03-20 20:31:00 -0400

    comments for columns

--- src/cljs/bulma_cljs_lib/columns/columns.cljs
+++ src/cljs/bulma_cljs_lib/columns/columns.cljs
@@ -1,7 +1,25 @@
 (ns bulma-cljs-lib.columns.columns)
 
-(defn columns [& content]
-  (into [:div.columns] content))
+;; sizes
+;is-three-quarters
+;is-two-thirds
+;is-half
+;is-one-third
+;is-one-quarter
+;is-full
+;The other columns will fill up the remaining space automatically.
+;
+;Since
+;0.6.1
+;You can now use the following multiples of 20% as well:
+;
+;is-four-fifths
+;is-three-fifths
+;is-two-fifths
+;is-one-fifth
 
-(defn column [& content]
-  (into [:div.column] content))
+(defn columns [props & content]
+  (into [:div.columns props] content))
+
+(defn column [props & content]
+  (into [:div.column props] content))
