642f9efb1de7bcc682df26f7b2c469f24d40e698
James Thibodeau <jthibaudeau@accedian.com>
2019-04-06 19:11:58 -0400

    prog-bar docs

--- src/cljs/bulma_cljs/docs/sub_views/elements/progress_bars.cljs
+++ src/cljs/bulma_cljs/docs/sub_views/elements/progress_bars.cljs
@@ -1,3 +1,44 @@
-(ns bulma-cljs.docs.sub-views.elements.progress-bars)
+(ns bulma-cljs.docs.sub-views.elements.progress-bars
+  (:require [bulma-cljs-lib.core :as b]
+            [reagent.core :as r]))
 
-(defn main [])
+(defn main []
+  (let [progress (r/atom 0)
+        max 10
+        increase-fn (fn []
+                      (swap! progress
+                             #(if (< % max)
+                                (inc %)
+                                %)))
+        decrease-fn (fn []
+                      (swap! progress
+                             #(if (> % 0)
+                                         (dec %)
+                                         %)))]
+    (fn []
+      [b/section
+       [b/subtitle "Progress Bars"]
+       [b/content "This is an example of the progress-bars component"]
+       [b/content
+        [b/button "Increase" increase-fn]
+        [b/button "Decrease" decrease-fn]]
+       [b/content
+        [b/box
+         [:p "Normal Progress Bar"]
+         [b/prog-bar "is-primary" @progress 10]]]
+       [b/content
+        [b/box
+         [:p "Small Progress Bar"]
+         [b/sm-prog-bar "is-info" @progress 10]]]
+       [b/content
+        [b/box
+         [:p "Medium Progress Bar"]
+         [b/md-prog-bar "is-warning" @progress 10]]]
+       [b/content
+        [b/box
+         [:p "Large Progress Bar"]
+         [b/lg-prog-bar "is-danger" @progress 10]]]
+       [b/content
+        [b/box
+         [:p "Indeterminate Progress Bar"]
+         [b/prog-bar "is-primary" nil 10]]]])))
