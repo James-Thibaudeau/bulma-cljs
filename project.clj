4cdf8ba5825c2f8436da5d2ae24b0a8cadae4c7e
James Thibodeau <jthibaudeau@accedian.com>
2019-05-07 20:32:12 -0400

    upversioned

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bulma-cljs "0.1.2"
+(defproject bulma-cljs "0.1.3"
   :description "A Reagent component library using Bulma"
   :url "https://github.com/James-Thibaudeau/bulma-cljs"
   :license {:name "MIT"}
